c003f16b1aa23ec6c6364ca3b93e5c56d01c6c81
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2022-09-14 18:42:22 -0400

    Finished ex00

---
+++ m00/ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	}
+	else {
+		for (int i = 1; argv[i]; ++i) {
+			for (int j = 0; argv[i][j]; ++j) {
+				std::cout << (char)toupper(argv[i][j]);
+			}
+		}
+	}
+	std::cout << "\n";
+	return 0;
+}
+ 
