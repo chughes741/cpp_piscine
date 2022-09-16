0ab88dadc6a2acfd6176379cab173181807688d9
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-16 15:14:57 -0400

    Added blank fn protos

---
+++ m00/ex02/Account.cpp
@@ -0,0 +1,43 @@
+
+
+#include "Account.hpp"
+
+int Account::getNbAccounts( void ) {
+	return 0;
+}
+
+int Account::getTotalAmount( void ) {
+	return 0;
+}
+
+int Account::getNbDeposits( void ) {
+	return 0;
+}
+
+int Account::getNbWithdrawals( void ) {
+	return 0;
+}
+
+void Account::displayAccountsInfos( void ) {
+	return;
+}
+
+void Account::makeDeposit( int depost ) {
+	return;
+}
+
+bool Account::makeWithdrawal( int withdrawl ) {
+	return false;
+}
+
+int Account::checkAmount( void ) const {
+	return 0;
+}
+
+void Account::displayStatus( void ) const {
+	return;
+}
+
+void Account::_displayTimestamp( void ) {
+	return;
+}
