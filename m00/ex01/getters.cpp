6232496a5cc39fc53230b4934de18d36c987a80d
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-16 13:38:29 -0400

    Made getters

---
+++ m00/ex01/getters.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+#include "PhoneBook.hpp"
+
+std::string Contact::getFirstName() {
+	return (first_name);
+}
+
+std::string Contact::getLastName() {
+	return (last_name);
+}
+
+std::string Contact::getNickname() {
+	return (nickname);
+}
