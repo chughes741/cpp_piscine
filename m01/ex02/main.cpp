29627dd5cdf20c626594c9d41e9ada8b6136b40d
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-17 11:26:06 -0400

    Finished m01/ex02

---
+++ m01/ex02/main.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream>
+#include <string>
+
+
+int main() {
+	
+	std::string hello = "HI THIS IS BRAIN";
+	std::string* ptr = &hello;
+	std::string& ref = hello;
+
+	std::cout	<< &hello	<< std::endl
+				<< ptr		<< std::endl
+				<< &ref		<< std::endl
+				<< hello	<< std::endl
+				<< *ptr		<< std::endl
+				<< ref		<< std::endl;
+
+	return 0;
+}
