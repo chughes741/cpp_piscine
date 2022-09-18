9ebdc84492b26f3934f0c37281db0526336f6dbd
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-18 13:36:36 -0400

    Updated phonebook header

--- m00/ex01/phonebook.hpp
+++ m00/ex01/phonebook.hpp
@@ -1,36 +1,53 @@
 
-#pragma once
-#ifndef H_PHONEBOOK
-# define H_PHONEBOOK
 
-# include <iomanip>
+#ifndef PHONEBOOK_H_
+# define PHONEBOOK_H_
+
 # include <iostream>
+# include <string>
+# include <iomanip>
+
 
 class Contact {
+	public:
+		Contact( void );
+		Contact( const Contact &contact);
+		~Contact();
+		Contact & operator = (const Contact &contact);
+
+		void		setFirstName();
+		void		setLastName();
+		void		setNickname();
+		void		setPhoneNumber();
+		void		setDarkestSecret();
+
+		std::string	getFirstName();
+		std::string	getLastName();
+		std::string	getNickname();
+	
 	private:
 		std::string first_name;
 		std::string last_name;
 		std::string nickname;
 		std::string	phone_number;
 		std::string darkest_secret;
-	public:
-		void		setFirstName();
-		void		setLastName();
-		void		setNickname();
-		void		setPhoneNumber();
-		void		setDarkestSecret();
-		std::string	getFirstName();
-		std::string	getLastName();
-		std::string	getNickname();
 };
 
 class PhoneBook {
+	public:
+		PhoneBook( void );
+		// PhoneBook(  );
+		PhoneBook( const PhoneBook &phonebook);
+		~PhoneBook();
+		PhoneBook & operator = (const PhoneBook &phonebook);
+
+		void	addContact();
+		void	searchContacts();
+
 	private:
 		int		n_contacts;
 		Contact contacts[8];
-	public:
-		void	addContact();
-		void	searchContacts();
 };
 
-#endif
+
+#endif // PHONEBOOK_H_
