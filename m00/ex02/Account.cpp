baaed1282d143a963e61b22dadb60d1fd0afea07
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-10-24 16:14:55 -0400

    Finished ex01

--- m00/ex02/Account.cpp
+++ m00/ex02/Account.cpp
@@ -2,42 +2,126 @@
 
 #include "Account.hpp"
 
+#include <iostream>
+
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+// Returns total number of accounts
 int Account::getNbAccounts( void ) {
-	return 0;
+	return (_nbAccounts);
 }
 
+// Returns total amount
 int Account::getTotalAmount( void ) {
-	return 0;
+	return (_totalAmount);
 }
 
+// Returns total number of deposits
 int Account::getNbDeposits( void ) {
-	return 0;
+	return (_totalNbDeposits);
 }
 
+// Returns total number of withdrawls
 int Account::getNbWithdrawals( void ) {
-	return 0;
+	return (_totalNbWithdrawals);
 }
 
+// TODO
 void Account::displayAccountsInfos( void ) {
+	_displayTimestamp();
+	std::cout << " accounts:" << _nbAccounts;
+	std::cout << ";total:" << _totalAmount;
+	std::cout << ";deposits:" << _totalNbDeposits;
+	std::cout << ";withdrawls:" << _totalNbWithdrawals << std::endl;
 	return;
 }
 
-void Account::makeDeposit( int depost ) {
+// Default constructor
+Account::Account( int initial_deposit ) {
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+	_accountIndex = _nbAccounts - 1;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex;
+	std::cout << ";amount:" << _amount;
+	std::cout << ";created" << std::endl;
 	return;
 }
 
+// Default destructor
+Account::~Account( void ) {
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex;
+	std::cout << ";amount:" << _amount;
+	std::cout << ";closed" << std::endl;
+	return;
+}
+
+// Increments private members with deposit amount
+void Account::makeDeposit( int deposit ) {
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex;
+	std::cout << ";p_amount:" << _amount;
+	std::cout << ";deposit:" << deposit;
+
+	_totalAmount += deposit;
+	_totalNbDeposits += 1;
+	_amount += deposit;
+	_nbDeposits += 1;
+
+	std::cout << ";amount:" << _amount;
+	std::cout << ";nb_deposits:" << _nbDeposits << std::endl;
+	return;
+}
+
+// Decrements private members with withdrawl amount if possible
 bool Account::makeWithdrawal( int withdrawl ) {
-	return false;
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex;
+	std::cout << ";p_amount:" << _amount;
+	std::cout << ";withdrawl:";
+	if (withdrawl > _amount) {
+		std::cout << "refused" << std::endl;
+		return false;
+	} else {
+		std::cout << withdrawl;
+
+	_totalAmount -= withdrawl;
+	_totalNbWithdrawals += 1;
+	_amount -= withdrawl;
+	_nbWithdrawals += 1;
+
+	std::cout << ";amount:" << _amount;
+	std::cout <<"nb_withdrawls:" << _nbWithdrawals << std::endl;
+	return true;
+	}
 }
 
+// Returns account balance
 int Account::checkAmount( void ) const {
-	return 0;
+	return (_amount);
 }
 
+// TODO
 void Account::displayStatus( void ) const {
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex;
+	std::cout << ";amount:" << _amount;
+	std::cout << ";deposits:" << _nbDeposits;
+	std::cout << ";withdrawls:" << _nbWithdrawals << std::endl;
 	return;
 }
 
+// TODO
 void Account::_displayTimestamp( void ) {
+	std::cout << "[19920104_091532]";
 	return;
 }
