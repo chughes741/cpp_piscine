fd616c63ee5a027e18913ebda08a1e181e9640f4
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-10-29 15:42:45 -0400

    Refactored m00/ex00 in a more "C++" way

--- m00/ex00/megaphone.cpp
+++ m00/ex00/megaphone.cpp
@@ -5,13 +5,14 @@ int main(int argc, char *argv[]) {
   if (argc == 1) {
     std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
   } else {
+    std::string copy;
     for (int i = 1; argv[i]; ++i) {
-      for (int j = 0; argv[i][j]; ++j) {
-        std::cout << (char)toupper(argv[i][j]);
-      }
+      copy = argv[i];
+      for (int c = 0; c < (int)copy.length(); c++)
+        copy[c] = toupper(copy[c]);
+      std::cout << copy;
     }
   }
   std::cout << std::endl;
-
   return 0;
 }
