47dbc6e0384d6387e12694eb272e5a3ed3cb0360
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-10-31 17:35:47 -0400

    Added ClapTrap class to m03/ex01

---
+++ m03/ex01/ClapTrap.hpp
@@ -0,0 +1,27 @@
+
+#ifndef CLAPTRAP_HPP_
+#define CLAPTRAP_HPP_
+
+#include <iostream>
+#include <string>
+
+// Class declaration
+class ClapTrap {
+ public:
+  ClapTrap(std::string name);
+  ClapTrap(const ClapTrap &claptrap);
+  ClapTrap &operator=(const ClapTrap &other);
+  ~ClapTrap();
+
+  void attack(const std::string &target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
+
+ private:
+  std::string name_;
+  unsigned int hit_points_;
+  unsigned int energy_points_;
+  unsigned int attack_damage_;
+};
+
+#endif  // CLAPTRAP_HPP_
