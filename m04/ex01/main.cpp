a2fed0d94af502eae3a2e8dd7eb8ee236b931782
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-11-10 16:28:49 -0500

    Removed WrongClasses and updated Makefile m04/ex01

--- m04/ex01/main.cpp
+++ m04/ex01/main.cpp
@@ -3,40 +3,8 @@
 #include "Animal.hpp"
 #include "Cat.hpp"
 #include "Dog.hpp"
-#include "WrongAnimal.hpp"
-#include "WrongCat.hpp"
 
 int main() {
-  {
-    const Animal* meta = new Animal();
-    meta->makeSound();
-    delete meta;
-    std::cout << std::endl;
-    
-    const Animal* i = new Cat();
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound();
-    delete i;
-    std::cout << std::endl;
 
-    const Animal* j = new Dog();
-    std::cout << j->getType() << " " << std::endl;
-    j->makeSound();
-    delete j;
-    std::cout << std::endl;
-  }
-  std::cout << std::endl;
-  {
-    const WrongAnimal* meta = new WrongAnimal();
-    meta->makeSound();
-    delete meta;
-    std::cout << std::endl;
-
-    const WrongAnimal* i = new WrongCat();
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound();
-    delete i;
-    std::cout << std::endl;
-  }
   return 0;
 }
