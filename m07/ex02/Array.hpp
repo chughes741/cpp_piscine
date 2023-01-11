fa9def477fe68ea29ecb99bf7a3bdd2657bb7a30
Cole Hughes <coltenhughes@gmail.com>
2023-01-11 12:06:28 -0500

    Fixed m07

--- m07/ex02/Array.hpp
+++ m07/ex02/Array.hpp
@@ -9,34 +9,34 @@
 template <class T>
 class Array {
  public:
-  Array() : size_(0), array_(new T[0]) {};
-  Array(unsigned size) : array_(new T[size]), size_(size) {};
+  Array() : size_(0), array_(new T[0]){};
+  Array(unsigned size) : size_(size), array_(new T[size]){};
   Array(const Array &other) : size_(other.size()), array_(new T[other.size()]) {
     for (std::size_t i = 0; i < size_; ++i) {
       array_[i] = other.array_[i];
     }
-	};
+  };
   Array &operator=(const Array &rhs) {
-    if (this == &rhs) return *this;
+    if (this != &rhs) {
+      delete[] array_;
+      size_  = rhs.size();
+      array_ = new T[size_];
+      for (std::size_t i = 0; i < size_; ++i) {
+        array_[i] = rhs.array_[i];
+      }
+    }
     return *this;
   };
-	T &operator[](std::size_t i) {
+  T &operator[](std::size_t i) {
     return i >= size_ ? throw std::range_error("Bad index") : array_[i];
-	};
-  ~Array(){
-    delete[] array_;
-	};
+  };
+  ~Array() { delete[] array_; };
 
-  std::size_t size() const {
-    return size_;
-	};
-	int	getValue(size_t i) {
-    return array_[i];
-	};
+  std::size_t size() const { return size_; };
 
  private:
-  T *array_;
   std::size_t size_;
+  T          *array_;
 };
 
 #endif  // ARRAY_HPP_
--- m07/ex02/main.cpp
+++ m07/ex02/main.cpp
@@ -5,13 +5,20 @@
 
 int main() {
   Array<int> int_array(10);
+  Array<int> copy(int_array);
   for (std::size_t i = 0; i < int_array.size(); ++i) {
     std::cout << int_array[i] << std::endl;
+    int_array[i] = i;
   }
   try {
     int_array[int_array.size()];
   } catch (std::range_error &e) {
     std::cout << e.what() << std::endl;
   }
+  copy = int_array;
+    for (std::size_t i = 0; i < int_array.size(); ++i) {
+    std::cout << copy[i] << std::endl;
+  }
+
   return 0;
 }
