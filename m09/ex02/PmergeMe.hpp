0c6223a14c331255408b7ebf3af35ead825f70cf
Cole Hughes <coltenhughes@gmail.com>
2023-06-05 10:46:46 -0400

    m09/ex02 finished

--- m09/ex02/PmergeMe.hpp
+++ m09/ex02/PmergeMe.hpp
@@ -1,71 +1,116 @@
 #pragma once
 
+#include <time.h>
+
+#include <cstdlib>
 #include <exception>
+#include <iomanip>
 #include <memory>
+#include <typeinfo>
 #include <utility>
 
-using std::allocator;
-using std::invalid_argument;
-using std::pair;
-using std::swap;
-
-/**
- * @brief Template for PmergeMe class
- *
- * @tparam T Underlying container type
- */
-template <template <class, class> class T>
+template <class T>
 class PmergeMe {
  public:
-  /**
-   * @brief Construct a new Pmerge Me object
-   *
-   * @param args Arguments to be sorted in the underlying container
-   */
-  PmergeMe(T<int> unsorted) {
-    // Check if the number of arguments is even, if not, add a -1 to the end
-    if (unsorted.size() % 2 != 0) {
-      unsorted.push_back(-1);
+  PmergeMe(char **argv) {
+    for (int i = 0; argv[i] != NULL; ++i) {
+      int value = std::atoi(argv[i]);
+      container_.push_back(std::make_pair(value, value));
     }
 
-    // Push pairs of arguments into the underlying container
-    for (T<int>::iterator it = unsorted.begin(); it != unsorted.end(); ++it) {
-      container_.push_back(pair<int, int>(*it, *(it + 1)));
-    }
-
-    // Sort the merged pairs
-    for (T<pair<int, int>>::iterator it = unsorted.begin(); it != unsorted.end(); ++it) {
-      if (it->first < it->second) {
-        swap(it->first, it->second);
-      }
-    }
+    mergeInsertSort();
   }
 
-  /**
-   * @brief Copy constructor
-   *
-   * @param other Other object
-   */
-  PmergeMe(const PmergeMe &other) { container_ = other.container_; }
-
-  /**
-   * @brief Copy assignment operator
-   *
-   * @param rhs Right hand side
-   * @return PmergeMe&
-   */
-  PmergeMe &operator=(const PmergeMe &rhs) {
-    if (this != &rhs) {
-      container_ = rhs.container_;
+  // Helper function to print the container
+  void printContainer() {
+    typename T::iterator it;
+    for (it = container_.begin(); it != container_.end(); ++it) {
+      std::cout << it->first << " ";
     }
-    return *this;
+    std::cout << "\n";
   }
-
-  /**
-   * @brief Destroy the Pmerge Me object
-   */
-  ~PmergeMe() { return; }
 
  private:
-  T<pair<int, int>, allocator<pair<int, int>>> container_; /** Underlying container */
+  // Merge-insert sort with timing
+  void mergeInsertSort() {
+    // Start time
+    clock_t start = clock();
+
+    // Sort
+    mergeInsertSortHelper(0, container_.size() - 1);
+
+    // End time
+    clock_t end     = clock();
+    double  elapsed = (end - start) / (double)CLOCKS_PER_SEC;
+
+    std::cout << "Sort time using " << container_type() << ": " << std::fixed << elapsed << "s"
+              << std::endl;
+  }
+
+  // Helper function for merge-insert sort
+  void mergeInsertSortHelper(int left, int right) {
+    if (left < right) {
+      int mid = left + (right - left) / 2;
+
+      mergeInsertSortHelper(left, mid);
+      mergeInsertSortHelper(mid + 1, right);
+      merge(left, mid, right);
+    }
+  }
+
+  // Helper function to merge two sorted subarrays
+  void merge(int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    std::vector<std::pair<int, int> > leftArr(n1);
+    std::vector<std::pair<int, int> > rightArr(n2);
+
+    for (int i = 0; i < n1; ++i) {
+      leftArr[i] = container_[left + i];
+    }
+
+    for (int j = 0; j < n2; ++j) {
+      rightArr[j] = container_[mid + 1 + j];
+    }
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+      if (leftArr[i].first <= rightArr[j].first) {
+        container_[k] = leftArr[i];
+        ++i;
+      } else {
+        container_[k] = rightArr[j];
+        ++j;
+      }
+      ++k;
+    }
+
+    while (i < n1) {
+      container_[k] = leftArr[i];
+      ++i;
+      ++k;
+    }
+
+    while (j < n2) {
+      container_[k] = rightArr[j];
+      ++j;
+      ++k;
+    }
+  }
+
+  // Helper function to determine container type in string form
+  std::string container_type() {
+    std::string type = typeid(T).name();
+    if (type.find("vector") != std::string::npos) {
+      return "vector";
+    } else if (type.find("deque") != std::string::npos) {
+      return "deque";
+    } else {
+      return "unknown";
+    }
+  }
+
+  T container_; /** Underlying container */
 };
--- m09/ex02/main.cpp
+++ m09/ex02/main.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include <deque>
+#include <set>
+#include <vector>
 
 #include "PmergeMe.hpp"
 
 int main(int argc, char *argv[]) {
-  PmergeMe<std::vector> pmergeMe(argv[1]);
-  return 0;
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " x y z..." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Print unsorted values
+  std::cout << "Presorted list: ";
+  for (int i = 1; i < argc; ++i) {
+    std::cout << argv[i] << " ";
+  }
+  std::cout << std::endl;
+
+  // Create set to check for duplicates and sort
+  std::set<int> set;
+  for (int i = 1; i < argc; ++i) {
+    // Convert to int and check for valid value
+    /** @todo use stoi */
+    long long value = std::atoll(argv[i]);
+    if (value < 0 || value > __INT_MAX__) {
+      std::cerr << "Invalid value: " << argv[i] << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    // Insert into set and check for duplicate
+    if (set.insert(value).second == false) {
+      std::cerr << "Duplicate value: " << argv[i] << std::endl;
+      return EXIT_FAILURE;
+    }
+  }
+
+  // Print sorted values
+  std::cout << "Sorted list: ";
+  for (std::set<int>::iterator it = set.begin(); it != set.end(); ++it) {
+    std::cout << *it << " ";
+  }
+  std::cout << std::endl;
+
+  PmergeMe<std::vector<std::pair<int, int> > > pmerge_vector(&argv[1]);
+  PmergeMe<std::deque<std::pair<int, int> > >   pmerge_deque(&argv[1]);
+
+  return EXIT_SUCCESS;
 }
